e0e4f78adfb2ca4eab6b71293afd1debeb12d6b2
Juliano Castilho <julianocomg@gmail.com>
2015-04-28 18:32:39 -0400

    And here we go!

---
+++ index.js
@@ -0,0 +1,41 @@
+
+/**
+ * @author Juliano Castilho <julianocomg@gmail.com>
+ */
+var UdpTransport = require('./transport/Udp');
+
+ /*
+ * @param {String} host
+ * @param {Number} port
+ */
+function Horus(host, port) {
+  var protocol = host.match(/(.[^:]+):\/\//)[1];
+
+  switch(protocol) {
+    case 'udp':
+      this.transport = new UdpTransport(host, port);
+      break;
+  }
+}
+ 
+Horus.prototype = {
+  /**
+   * @param {Object} payload
+   */
+  send: function(payload) {
+    var tags = payload.tags;
+    var message = payload.message;
+
+    if (!tags)
+      return this.transport.send(message);
+
+    if (tags && tags.length)
+      var self = this;
+
+      tags.map(function(tag) {
+        self.transport.send(tag + '\0' + message);
+      });
+  }
+};
+
+module.exports = Horus;
